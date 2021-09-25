83dacb4dc77d54fcd0ad5c9cfa33c9c042f7e4a1
Mayya Peneva <maya.peneva@gmail.com>
2021-09-25 17:46:18 +0300

    Add files via upload

---
+++ ChatSystem.App/Pages/Chat.cs
@@ -0,0 +1,43 @@
+﻿namespace ChatSystem.App.Pages
+{
+    using Contracts;
+    using Microsoft.AspNetCore.Components;
+    using Models;
+    using System.Collections.Generic;
+
+    public partial class Chat
+    {
+        [Inject]
+        public IMessageService MessageService { get; set; }
+
+        [Parameter]
+        public Message Message { get; set; }
+
+        public string Author { get; set; }
+
+        public List<Message> Messages { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Message = new Message();
+            Messages = new List<Message>(MessageService.GetLastMessages());
+        }
+
+        protected void Create()
+        {
+            if (Message.Author != null)
+            {
+                Author = Message.Author;
+            }
+
+            Messages.Add(Message);
+            if (Messages.Count > 20)
+            {
+                Messages.RemoveAt(0);
+            }
+
+            Message = new Message();
+            Message.Author = Author;
+        }
+    }
+}
---
+++ ChatSystem.App/Services/MessageService.cs
@@ -0,0 +1,14 @@
+﻿namespace ChatSystem.App.Services
+{
+    using Contracts;
+    using Models;
+    using System.Collections.Generic;
+
+    internal class MessageService : IMessageService
+    {
+        public IEnumerable<Message> GetLastMessages()
+        {
+            return new List<Message>();
+        }
+    }
+}
